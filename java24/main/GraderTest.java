b6167d1a9384eaf5426bb6432dc0565be1bec6c7
joeryan <joe@clanryan.us>
2013-12-03 03:35:27 +0430

    add error handling and tests for error handling

--- java24/main/GraderTest.java
+++ java24/main/GraderTest.java
@@ -7,9 +7,21 @@ import org.junit.Test;
 public class GraderTest {
 
 	@Test
-	public void test_getGrade() {
+	public void test_gradeA() {
 		Grader myGrade = new Grader(90);
 		assertEquals('A', myGrade.getGrade());
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testGradeGreaterThan100() {
+		Grader myGrade = new Grader(101);
+		System.out.println(myGrade.getGrade());
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testGradeLessThan0() {
+		Grader myGrade = new Grader(-1);
+		System.out.println(myGrade.getGrade());
 	}
 
 }
