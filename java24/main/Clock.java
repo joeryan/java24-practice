286a43f2ec6fefa6ca4df2f02ca785f1f86a7c71
joeryan <joe@clanryan.us>
2013-12-03 02:25:24 +0430

    refactor Clock to add constructor and initial displayGreeting()

--- java24/main/Clock.java
+++ java24/main/Clock.java
@@ -3,76 +3,49 @@ package java24.main;
 import java.util.*;
 
 public class Clock {
+	private int hour;
+	private int minute;
+	private int month;
+	private int day;
+	private int year;
+	
+	public Clock() {
+		Calendar now = Calendar.getInstance();
+		this.hour = now.get(Calendar.HOUR_OF_DAY);
+		this.minute = now.get(Calendar.MINUTE);
+		this.month = now.get(Calendar.MONTH);
+		this.day = now.get(Calendar.DAY_OF_MONTH);
+		this.year = now.get(Calendar.YEAR);
+	}
+		
 	public static void main(String[] args) {
-			Calendar now = Calendar.getInstance();
-			int hour = now.get(Calendar.HOUR_OF_DAY);
-			int minute = now.get(Calendar.MINUTE);
-			int month = now.get(Calendar.MONTH)+1;
-			int day = now.get(Calendar.DAY_OF_MONTH);
-			int year = now.get(Calendar.YEAR);
-			
-			if (hour < 12) {
-				System.out.println("Good morning. \n");
-			} else if (hour < 17) {
-				System.out.println("Good afternoon.\n");
-			} else {
-				System.out.println("Good evening.\n");
-			}
-			
-			System.out.print("It's ");
-			if (minute != 0) {
-				System.out.print(minute + " ");
-				System.out.print( (minute != 1) ? "minutes" : "minute");
-				System.out.print(" past ");
-			}
-			System.out.print ( ( hour > 12) ? (hour - 12) : hour);
-			System.out.print( " o'clock on ");
-			
-			switch (month) {
-				case 1:
-					showMonth("January");
-					break;
-				case 2:
-					showMonth("February");
-					break;
-				case 3:
-					showMonth("March");
-					break;
-				case 4:
-					showMonth("April");
-					break;
-				case 5:
-					showMonth("May");
-					break;
-				case 6:
-					showMonth("June");
-					break;
-				case 7:
-					showMonth("July");
-					break;
-				case 8:
-					showMonth("August");
-					break;
-				case 9:
-					showMonth("September");
-					break;
-				case 10:
-					showMonth("October");
-					break;
-				case 11:
-					showMonth("November");
-					break;
-				case 12:
-					showMonth("December");
-					break;
-				default:
-					System.out.println("Error!!");
-			}
-			
-			System.out.println(" " + day + ", " + year + ".");
+		Clock myClock = new Clock(Calendar.getInstance());
+		myClock.displayGreeting();
 	}
 	
-	private static void showMonth(String month) {
-		System.out.print(month);
+	protected void displayGreeting() {
+		if (hour < 12) {
+			System.out.println("Good morning. \n");
+		} else if (hour < 17) {
+			System.out.println("Good afternoon.\n");
+		} else {
+			System.out.println("Good evening.\n");
+		}
+			
+		System.out.print("It's ");
+		if (minute != 0) {
+			System.out.print(minute + " ");
+			System.out.print( (minute != 1) ? "minutes" : "minute");
+			System.out.print(" past ");
+		}
+		System.out.print ( ( hour > 12) ? (hour - 12) : hour);
+		System.out.print( " o'clock on ");
+		System.out.println(this.showMonth() + " " + day + ", " + year + ".");
+	}
+	
+	private String showMonth() {
+		String months[] = {"January", "February", "March", "April", "May", "June", "July",
+		                   "August", "September", "October", "November", "December"};
+		return months[this.month];
 	}
 }
