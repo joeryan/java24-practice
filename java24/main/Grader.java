50f02867510e6ed9b5d9f91fc2b811316c8ae55d
joeryan <joe@clanryan.us>
2013-12-03 03:14:13 +0430

    create simple Grader class and GraderTest

---
+++ java24/main/Grader.java
@@ -0,0 +1,23 @@
+package java24.main;
+
+public class Grader {
+	private int grade;
+	
+	public Grader(int grade) {
+		this.grade = grade;
+	}
+	
+	public String getGrade() {
+		if (grade >= 90) {
+			return 'A';
+		} else if (grade >= 80) {
+			return 'B';
+		} else if (grade >= 70) {
+			return 'C';
+		} else if (grade >= 60) {
+			return 'D';
+		} else {
+			return 'E';
+		}
+	}
+}
---
+++ java24/main/GraderTest.java
@@ -0,0 +1,14 @@
+package java24.main;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GraderTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
