de67833a8d6364e45c9666a75b7d861ab36e819c
joeryan <joe@clanryan.us>
2013-12-03 04:36:10 +0430

    add formatting to Loops.forLoopNines()

--- java24/main/Loops.java
+++ java24/main/Loops.java
@@ -1,14 +1,20 @@
 package java24.main;
 
 public class Loops {
-	public static void forLoopNines() {
-		for (int multiplier = 1; multiplier < 100; multiplier++) {
-			int multiple = 9 * multiplier;
-			System.out.print(multiple + " ");
-		}
-	}
+	
 	
 	public static void main(String[] args) {
 		Loops.forLoopNines();
 	}
+	
+	protected static void forLoopNines() {
+		for (int multiplier = 1; multiplier < 100; multiplier++) {
+			int multiple = 9 * multiplier;
+			System.out.print(multiple + " ");
+			if (multiplier% 20 == 0) {
+				System.out.println("\n");
+			}
+		}
+	}
+		
 }
