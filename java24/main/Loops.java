f4d95de84c85598c156618b702da1caaeffe9495
joeryan <joe@clanryan.us>
2013-12-03 04:41:37 +0430

    add Loops.whileLoop() to show while(){}

--- java24/main/Loops.java
+++ java24/main/Loops.java
@@ -5,6 +5,7 @@ public class Loops {
 	
 	public static void main(String[] args) {
 		Loops.forLoopNines();
+		Loops.whileLoop();
 	}
 	
 	protected static void forLoopNines() {
@@ -12,9 +13,18 @@ public class Loops {
 			int multiple = 9 * multiplier;
 			System.out.print(multiple + " ");
 			if (multiplier% 20 == 0) {
-				System.out.println("\n");
+				System.out.print("\n");
 			}
 		}
+		System.out.println();
 	}
-		
+	
+	protected static void whileLoop() {
+		int limit = 5;
+		int count = 1;
+		while (count < limit) {
+			System.out.println("This is line number " + count + ".");
+			count ++;
+		}
+	}
 }
