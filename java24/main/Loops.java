d98db5d6edcb0fb635e94f10bfa8ba5953ec41b6
joeryan <joe@clanryan.us>
2013-12-03 04:48:05 +0430

    add Loops.doWhileLoop() showing do-while minimum execution

--- java24/main/Loops.java
+++ java24/main/Loops.java
@@ -6,9 +6,11 @@ public class Loops {
 	public static void main(String[] args) {
 		Loops.forLoopNines();
 		Loops.whileLoop();
+		Loops.doWhileLoop();
 	}
 	
 	protected static void forLoopNines() {
+		System.out.println("for loops for 100 iterations");
 		for (int multiplier = 1; multiplier < 100; multiplier++) {
 			int multiple = 9 * multiplier;
 			System.out.print(multiple + " ");
@@ -22,9 +24,21 @@ public class Loops {
 	protected static void whileLoop() {
 		int limit = 5;
 		int count = 1;
+		System.out.println("while loop for 5 iterations");
 		while (count < limit) {
 			System.out.println("This is line number " + count + ".");
 			count ++;
 		}
+	}
+	
+	protected static void doWhileLoop() {
+		int limit = 5;
+		int count = 6;
+		
+		System.out.println("do-while loop with one iteration showing 'execution-before-test'");
+		do {
+			System.out.println("This will only print when " + count + " is less than " + limit);
+			count ++;
+		} while (count < limit);
 	}
 }
